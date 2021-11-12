459aa059f3eee1476ce4287006aa38e3262dd90d
Veronika Hunyak <veronika.hunyak@sky.uk>
2021-11-12 13:00:20 +0000

    week1-basics-exercise6

---
+++ week1/basics/6/MyVars.h
@@ -0,0 +1,9 @@
+// Name the file MyVars.h and click the add button.
+// Within the new file define a variable named surname of type string and assign it your surname.
+// You will need to include the <string> library and indicate that you are using the std namespace.
+// Make the changes necessary to display the variable from within the main function.
+
+#include <string>
+using namespace std;
+
+string surname = "Eve";
---
+++ week1/basics/6/e6.cpp
@@ -0,0 +1,10 @@
+
+#include "MyVars.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The surname held in MyVars.h is: " << surname << endl;
+}
