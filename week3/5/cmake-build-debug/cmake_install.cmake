a4ac8f7cec2fe5ebfcd4499d8a9857445a14bf13
Veronika <hunyakvera@gmail.com>
2022-01-03 18:04:46 +0000

    week3 e5 stuff

--- week3/5/cmake-build-debug/cmake_install.cmake
+++ week3/5/cmake-build-debug/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/veronikaeve/Brainjuice/LearnCpp/week3/5&6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/veronikaeve/Brainjuice/LearnCpp/week3/5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
