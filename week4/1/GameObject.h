340f8197eadbc41e9aeca82fd0b5565bf54e39b6
Veronika <hunyakvera@gmail.com>
2022-01-03 18:04:21 +0000

    week4 e1

---
+++ week4/1/GameObject.h
@@ -0,0 +1,14 @@
+#ifndef INC_1_GAMEOBJECT_H
+#define INC_1_GAMEOBJECT_H
+
+class GameObject {
+public:
+    int id;
+
+    GameObject() {
+        id=1;
+    }
+};
+
+
+#endif //INC_1_GAMEOBJECT_H
---
+++ week4/1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "GameObject.h"
+
+using namespace std;
+
+int main() {
+    GameObject obj1;
+
+    cout << obj1.id << endl;
+}
