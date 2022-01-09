b390be265c778bbbf1590576f5896419675c84d2
Veronika <hunyakvera@gmail.com>
2022-01-09 16:34:41 +0000

    e3 - added an array of numbers

--- week7/FunctionPointersAndTemplates/main.cpp
+++ week7/FunctionPointersAndTemplates/main.cpp
@@ -14,6 +14,17 @@ void Message(const string param){
 	cout << param << endl;
 }
 
+void Sort(int *numbers, int size) { for (int n= 0; n < size; n++) {
+		int nBestIndex = n;
+		for (int i = n + 1; i < size; i++){
+			if (numbers[i] < numbers[nBestIndex]) nBestIndex = i;
+		}
+		swap(numbers[n], numbers[nBestIndex]);
+	}
+	for (int i=0; i < size; i++)
+		cout << numbers[i] << endl;
+}
+
 int main() {
 	void(*message)();
 	message=HelloMessage;
@@ -24,5 +35,8 @@ int main() {
 	void(*newMessage)(string);
 	newMessage=Message;
 	newMessage("Bunny");
+
+	int numbers[9] ={ 3, 7, 9, 5, 6, 1, 8, 2, 4 };
+	Sort(numbers,9);
 
 }
